fd2e73651d28dd1f514135989e3577872d3e9162
Andreas Riegler <andreas-riegler@hotmail.com>
2015-04-07 23:24:55 +0200

    implemented addTerms()

--- information_retrieval_ex1/src/main/java/searcher/index/BagOfWordsIndex.java
+++ information_retrieval_ex1/src/main/java/searcher/index/BagOfWordsIndex.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 
 public class BagOfWordsIndex extends AbstractIndex{
 
-	
+
 	public BagOfWordsIndex() {
 		super();
 	}
 
 	@Override
 	public void addTerms(ArrayList<String> terms) {
-		// TODO Auto-generated method stub
-		
-	}
 
+		for(String term : terms){
+
+			if(index.containsKey(term)){
+				index.put(term, index.get(term) + 1);
+			}
+			else{
+				index.put(term, 1);
+			}
+		}
+	}
 }
--- information_retrieval_ex1/src/main/java/searcher/index/BiwordIndex.java
+++ information_retrieval_ex1/src/main/java/searcher/index/BiwordIndex.java
@@ -10,8 +10,19 @@ public class BiwordIndex extends AbstractIndex{
 
 	@Override
 	public void addTerms(ArrayList<String> terms) {
-		// TODO Auto-generated method stub
-		
-	}
 
+		String [] termsArray = (String []) terms.toArray();
+
+		for(int i = 0; i < terms.size() - 1; i++){
+
+			String biword = termsArray[i] + " " + termsArray[i + 1];
+
+			if(index.containsKey(biword)){
+				index.put(biword, index.get(biword) + 1);
+			}
+			else{
+				index.put(biword, 1);
+			}		
+		}	
+	}
 }
