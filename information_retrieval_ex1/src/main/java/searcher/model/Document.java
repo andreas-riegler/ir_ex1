f6753b51f3a63ff4a71b18527abbf9b1897e8e27
Andreas Riegler <andreas-riegler@hotmail.com>
2015-04-07 22:47:52 +0200

    updated Document

--- information_retrieval_ex1/src/main/java/searcher/model/Document.java
+++ information_retrieval_ex1/src/main/java/searcher/model/Document.java
@@ -1,5 +1,26 @@
 package searcher.model;
 
+import java.util.Map;
+
 public class Document {
 
+	private String documentId;	
+	private Map<String, Integer> documentIndex;
+	
+	
+	public String getDocumentId() {
+		return documentId;
+	}
+	
+	public void setDocumentId(String documentId) {
+		this.documentId = documentId;
+	}
+	
+	public Map<String, Integer> getDocumentIndex() {
+		return documentIndex;
+	}
+	
+	public void setDocumentIndex(Map<String, Integer> documentIndex) {
+		this.documentIndex = documentIndex;
+	}
 }
